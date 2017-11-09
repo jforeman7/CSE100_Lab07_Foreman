8b31be405d675fed715ccb379c186324c06bfb2f
jforeman7 <31979772+jforeman7@users.noreply.github.com>
2017-11-08 21:38:16 -0800

    Update Huffman.cpp

--- Huffman.cpp
+++ Huffman.cpp
@@ -2,91 +2,110 @@
 // CSE 100 03L
 // Lab 07: Huffman Codes
 // 30 Oct 2017
-// Changed my heapsort code to create a MinHeap.
-
 #include <iostream>
-#include <stdlib.h>
+#include <algorithm>
+#include <vector>
+#include <queue>
 #include <string>
-#include <utility> // For Swap.
 
-using namespace std;
-
-// Must add 1, to account for change in index start from textbook pseudocode.
-int Parent(int i) { return i / 2; }
-int Left(int i) { return 2 * i + 1; }
-int Right(int i) { return 2 * i + 2; }
-
-void MinHeapify(int* A, int A_size, int i)
+class HuffmanCodes
 {
-	int l = Left(i);
-	int r = Right(i);
-	int smallest;
+ struct Node
+ {
+  char data;
+  size_t freq;
+  Node* left;
+  Node* right;
+  Node(char data, size_t freq) : data(data),
+                                 freq(freq),
+                                 left(nullptr),
+                                 right(nullptr)
+                                 {}
+ ~Node()
+ {
+   delete left;
+   delete right;
+ }
+ };
 
-	// Look for the smallest value, and store its index value in "smallest".
-	if (l < A_size && A[l] < A[i])
-		smallest = l;
-	else
-		smallest = i;
+ struct compare
+ {
+  bool operator()(Node* l, Node* r)
+  {
+    return (l->freq > r->freq);
+  }
+};
 
-	if (r < A_size && A[r] < A[smallest])
-		smallest = r;
+Node* top;
 
-	// If A[i] is smallest, then done.
-	// Otherwise we must swap and then start again, as the max heap property may be violated.
-	if (smallest != i)
-	{
-		swap(A[i], A[smallest]);
+void printCode(Node* root, std::string str)
+{
+  if(root == nullptr)
+   return;
 
-		MinHeapify(A, A_size, smallest);
-	}
+ if(root->data == '$')
+ {
+  printCode(root->left, str + "0");
+  printCode(root->right, str + "1");
+ }
+
+ if(root->data != '$')
+ {
+   std::cout << root->data << " : " << str << "\n";
+   printCode(root->left, str + "0");
+   printCode(root->right, str + "1");
+ }
 }
 
-void BuildMinHeap(int* A, int A_size)
-{
-	// Ensures smallest value is at root, to satisfy max heap property.
-	for(int i = A_size / 2 - 1; i >= 0; i--)
-		MinHeapify(A, A_size, i);
-}
+public:
+  HuffmanCodes() {};
+  ~HuffmanCodes()
+  {
+    delete top;
+  }
+  void GenerateCode(std::vector<char>& data, std::vector<size_t>& freq, size_t size)
+  {
+   Node* left;
+   Node* right;
+   std::priority_queue<Node*, std::vector<Node*>, compare > minHeap;
 
-void HeapSort(int* A, int A_size)
-{
-	BuildMinHeap(A, A_size);
+   for(size_t i = 0; i < size; ++i)
+   {
+      minHeap.push(new Node(data[i], freq[i]));
+   }
 
-	for(int i = A_size - 1; i >= 1; i--)
-	{
-		swap(A[0], A[i]);
+    while(minHeap.size() != 1)
+    {
+      left = minHeap.top();
+      minHeap.pop();
 
-		//Largest value is now at the end, so "cut" off that node.
-		A_size--;
+      right = minHeap.top();
+      minHeap.pop();
 
-		// Re-Heap.
-		MinHeapify(A, A_size, 0);
-	}
-}
+      top = new Node('$', left->freq + right->freq);
+      top->left  = left;
+      top->right = right;
+      minHeap.push(top);
+     }
+    printCode(minHeap.top(), "");
+ }
+};
 
+ int main()
+ {
+  HuffmanCodes set1;
+	 int size;
+	 cin >> size;
+	 
+  std::vector<char> data;
+  std::vector<size_t> freq;
+	 
+	 for (int i = 0; i < size; i++)
+	 {
+			data[i] = 'a';
+		 	cin >> freq[i];
+	 }
+  set1.GenerateCode(data, freq, size);
 
-int main(int argc,char **argv) {
-
-  int *Sequence;
-  int arraySize;
-
-  // Get the size of the sequence
-  cin >> arraySize;
-
-  // Allocate enough memory to store "arraySize" integers
-  Sequence = new int[arraySize];
-
-  // Read in the sequence
-  for ( int i=0; i<arraySize; i++ )
-    cin >> Sequence[i];
-
-  // Run your algorithms to manipulate the elements in Sequence
-  HeapSort(Sequence, arraySize);
-
-  // Output the result
-  for(int i=0; i<arraySize; i++)
-      cout << Sequence[i] << endl;
-
-  // Free allocated space
-  delete[] Sequence;
+  return 0;
 }
